d03aa150c7625238936bdada8a61721297015bcc
cf.le3 <cf.lee0064@gmail.com>
2025-05-22 16:51:50 +0800

    Update "MainActivity" class
    Implemented functionality to initialize a CameraX instance and take pictures.

--- app/src/main/java/com/example/calmsidocr/MainActivity.java
+++ app/src/main/java/com/example/calmsidocr/MainActivity.java
@@ -1,56 +1,59 @@
 package com.example.calmsidocr;
 
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.impl.utils.Exif;
-import androidx.exifinterface.media.ExifInterface;
 
-import androidx.core.content.FileProvider;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.text.Text;
-import com.google.mlkit.vision.text.TextRecognition;
-import com.google.mlkit.vision.text.TextRecognizer;
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
+/** @noinspection ResultOfMethodCallIgnored*/
 public class MainActivity extends AppCompatActivity {
 
     // View data
-    private Button btnCamera;
-    private Button btnClear;
-    private Button btnParse;
-    private ImageView imgCapture;
-    private LinearLayout linearResults;
-    private TextView txtResult;
-    private ActivityResultLauncher<Uri> launcherCamera;
+    private PreviewView previewCamera;
+    private FloatingActionButton btnCapture;
+    private Button btnRetry;
+    private Button btnSubmit;
+    private ImageView imagePhoto;
+    private ViewGroup layoutResult;
+
+    // CameraX
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ProcessCameraProvider cameraProvider;
+    private CameraSelector cameraSelector;
+    private Preview preview;
+    private ImageCapture imageCapture;
+    private Camera camera;
 
     // Non-view data
     private File fileCapture;
-    private Uri uriCapture;
-    private ViewState viewState;
-
-    // Instance state bundle keys
-    private final String KEY_FILE = "KEY_FILE";
-    private final String KEY_URI = "KEY_URI";
-    private final String KEY_STATE = "KEY_STATE";
+    private static final String TAG_CALLBACK = "CALLBACK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,80 +69,78 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Bind View components.
-        btnCamera = findViewById(R.id.btnCamera);
-        btnClear = findViewById(R.id.btnClear);
-        btnParse = findViewById(R.id.btnParse);
-        imgCapture = findViewById(R.id.imageCapture);
-        linearResults = findViewById(R.id.linearResults);
-        txtResult = findViewById(R.id.txtResultTest);
+        previewCamera = findViewById(R.id.previewCamera);
+        btnCapture = findViewById(R.id.btnCapture);
+        btnRetry = findViewById(R.id.btnRetry);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        imagePhoto = findViewById(R.id.imagePhoto);
+        layoutResult = findViewById(R.id.layoutResult);
 
-        launcherCamera = registerForActivityResult(
-                new ActivityResultContracts.TakePicture(),
-                isSuccess -> {
-                    if (isSuccess) {
-                        Glide.with(this).load(uriCapture).into(imgCapture);
-                        viewState = setViewVisibility(ViewState.CAPTURED);
-                    }
-                }
-        );
+        setupCamera();
 
-        btnCamera.setOnClickListener(view -> {
+        btnCapture.setOnClickListener(view -> {
             try {
-                // Define path for new temporary file.
                 fileCapture = getFilePath();
+                ImageCapture.OutputFileOptions outputFileOptions =
+                        new ImageCapture.OutputFileOptions.Builder(fileCapture)
+                                .build();
+                imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this),
+                        new ImageCapture.OnImageSavedCallback() {
+                            @Override
+                            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                                Glide.with(view.getContext()).load(fileCapture).into(imagePhoto);
+                                setViewVisibility(ViewState.AFTER_CAPTURE);
+                                Log.d(TAG_CALLBACK, "CALLBACK: takePicture onImageSaved");
+                            }
 
-                // Derive URI from given path.
-                uriCapture = FileProvider.getUriForFile(
-                        this,
-                        getPackageName() + ".fileprovider",
-                        fileCapture
-                );
+                            @Override
+                            public void onError(@NonNull ImageCaptureException exception) {
+                                Log.w(TAG_CALLBACK, "CALLBACK: takePicture onError");
+                            }
+                        });
+                Log.d(TAG_CALLBACK, "CALLBACK: btnCapture onClick");
+            } catch (Exception e){
+                Log.e(TAG_CALLBACK, "EXCEPTION: btnCapture onClick");
+            }
+        });
 
-                launcherCamera.launch(uriCapture);
-            } catch (IOException e) {
+        btnRetry.setOnClickListener(view -> {
+            Glide.with(view.getContext()).clear(imagePhoto);
+            setViewVisibility(ViewState.BEFORE_CAPTURE);
+            fileCapture.delete();
+            Log.d(TAG_CALLBACK, "CALLBACK: btnRetry onClick");
+        });
+
+        btnSubmit.setOnClickListener(view -> {
+            Toast.makeText(this, (CharSequence) "Coming soon...", Toast.LENGTH_SHORT).show();
+            Log.d(TAG_CALLBACK, "CALLBACK: btnSubmit onClick");
+        });
+
+        Log.d(TAG_CALLBACK, "CALLBACK: MainActivity onCreate");
+    }
+
+    private void setupCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                cameraProvider = cameraProviderFuture.get();
+                cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
+                imageCapture = new ImageCapture.Builder()
+                        .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                        .build();
+                preview = new Preview.Builder()
+                        .build();
+                preview.setSurfaceProvider(previewCamera.getSurfaceProvider());
+                camera = cameraProvider.bindToLifecycle(
+                        this, cameraSelector, imageCapture, preview);
+                Log.d(TAG_CALLBACK, "CALLBACK: cameraProviderFuture listener");
+            } catch (ExecutionException | InterruptedException e) {
                 e.printStackTrace();
+                Log.e(TAG_CALLBACK, "EXCEPTION: cameraProviderFuture listener");
             }
-        });
-
-        btnClear.setOnClickListener(view -> {
-            Glide.with(view.getContext()).clear(imgCapture);
-            viewState = setViewVisibility(ViewState.CLEARED);
-            System.out.println(fileCapture.delete() ? "File deleted." : "File not deleted.");
-        });
-
-        btnParse.setOnClickListener(view -> {
-            if (viewState != ViewState.PARSED) {
-                try {
-                    ExifInterface exif = new ExifInterface(fileCapture);
-                    txtResult.setText(String.valueOf(exif.getRotationDegrees()));
-                    viewState = setViewVisibility(ViewState.PARSED);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        if (fileCapture != null) outState.putString(KEY_FILE, fileCapture.getAbsolutePath());
-        if (uriCapture != null) outState.putString(KEY_URI, uriCapture.toString());
-        if (viewState != null) outState.putString(KEY_STATE, viewState.name());
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        String strFile = savedInstanceState.getString(KEY_FILE);
-        String strUri = savedInstanceState.getString(KEY_URI);
-        String strState = savedInstanceState.getString(KEY_STATE);
-        if (strFile != null) fileCapture = new File(strFile);
-        if (strUri != null) {
-            uriCapture = Uri.parse(strUri);
-            Glide.with(this).load(uriCapture).into(imgCapture);
-        }
-        if (strState != null) viewState = setViewVisibility(ViewState.valueOf(strState));
-        super.onRestoreInstanceState(savedInstanceState);
+        }, ContextCompat.getMainExecutor(this));
     }
 
     private File getFilePath() throws IOException {
@@ -148,12 +149,8 @@ public class MainActivity extends AppCompatActivity {
         return File.createTempFile("capture_", ".jpg", targetDir);
     }
 
-    private ViewState setViewVisibility(ViewState state) {
-        btnCamera.setVisibility(state == ViewState.CLEARED ? View.VISIBLE : View.INVISIBLE);
-        imgCapture.setVisibility(state != ViewState.CLEARED ? View.VISIBLE : View.INVISIBLE);
-        linearResults.setVisibility(state == ViewState.PARSED ? View.VISIBLE : View.INVISIBLE);
-        return state;
+    private void setViewVisibility(ViewState state) {
+        btnCapture.setVisibility(state == ViewState.BEFORE_CAPTURE ? View.VISIBLE : View.INVISIBLE);
+        layoutResult.setVisibility(state == ViewState.AFTER_CAPTURE ? View.VISIBLE : View.INVISIBLE);
     }
-
-
 }
