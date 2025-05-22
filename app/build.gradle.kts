51f5028b7e8adc5042bb4839814bf19f9a80d224
cf.le3 <cf.lee0064@gmail.com>
2025-05-22 16:52:47 +0800

    Update build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,16 +36,19 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.github.glide)
     implementation(libs.text.recognition)
-    implementation(libs.core.ktx)
     implementation(libs.camera.core)
+    implementation(libs.camera.camera2)
     implementation(libs.camera.lifecycle)
+    implementation(libs.camera.video)
+    implementation(libs.camera.view)
+    implementation(libs.camera.extensions)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
