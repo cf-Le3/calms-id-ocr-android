493303be99695a8c219dc2cc1aaabe3e95b644a2
cf.le3 <cf.lee0064@gmail.com>
2025-05-26 16:23:39 +0800

    Migrate tag and key constants to separate class

---
+++ app/src/main/java/com/example/calmsidocr/Constants.java
@@ -0,0 +1,9 @@
+package com.example.calmsidocr;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final String KEY_FILE = "KEY_F";
+    public static final String TAG_EVENT = "TAG_E";
+    public static final String TAG_BUNDLE = "TAG_B";
+}
