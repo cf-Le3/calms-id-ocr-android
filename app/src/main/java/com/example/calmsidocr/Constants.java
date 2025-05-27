cfe89bc13934b6e408177e06595295a9e66a3a4d
cf.le3 <cf.lee0064@gmail.com>
2025-05-27 15:46:52 +0800

    Add new constant strings for NRIC Regex and message if match not found

--- app/src/main/java/com/example/calmsidocr/Constants.java
+++ app/src/main/java/com/example/calmsidocr/Constants.java
@@ -4,6 +4,8 @@ public final class Constants {
     private Constants() {}
 
     public static final String KEY_FILE = "KEY_F";
+    public static final String PAT_NRIC = "\\d\\d\\d\\d\\d\\d-\\d\\d-\\d\\d\\d\\d";
+    public static final String MSG_NA = "Not found.";
     public static final String TAG_EVENT = "TAG_E";
     public static final String TAG_BUNDLE = "TAG_B";
 }
