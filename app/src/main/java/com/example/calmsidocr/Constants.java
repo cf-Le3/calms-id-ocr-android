29cdebe78909de7de45de7939ab51dea33e21e25
cf.le3 <cf.lee0064@gmail.com>
2025-05-29 12:58:02 +0800

    Define new Regex patterns for NRIC and Passport number

--- app/src/main/java/com/example/calmsidocr/Constants.java
+++ app/src/main/java/com/example/calmsidocr/Constants.java
@@ -4,7 +4,8 @@ public final class Constants {
     private Constants() {}
 
     public static final String KEY_FILE = "KEY_F";
-    public static final String PAT_NRIC = "\\d\\d\\d\\d\\d\\d-\\d\\d-\\d\\d\\d\\d";
+    public static final String PAT_NRIC = "([0-9]{6}-[0-9]{2}-[0-9]{4}|[0-9]{12})";
+    public static final String PAT_PASSPORT = "[A-Z]*[0-9]+[A-Z]*";
     public static final String MSG_NA = "Not found.";
     public static final String TAG_EVENT = "TAG_E";
     public static final String TAG_BUNDLE = "TAG_B";
