737dcd1468f4e2dbce399443cad71d8ccdf87449
cf.le3 <cf.lee0064@gmail.com>
2025-05-29 12:57:24 +0800

    Implement face detection and extraction functionality

--- app/src/main/java/com/example/calmsidocr/ImageParser.java
+++ app/src/main/java/com/example/calmsidocr/ImageParser.java
@@ -1,18 +1,16 @@
 package com.example.calmsidocr;
 
 import android.content.Context;
-import android.media.Image;
 import android.net.Uri;
 
-import androidx.annotation.NonNull;
-import androidx.camera.core.Camera;
 import androidx.core.content.FileProvider;
-import androidx.exifinterface.media.ExifInterface;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
@@ -20,13 +18,19 @@ import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class ImageParser {
     TextRecognizer recognizer;
+    FaceDetector detector;
     InputImage image;
 
     public ImageParser(Context context, File fileCapture) throws IOException {
         recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        FaceDetectorOptions options = new FaceDetectorOptions.Builder()
+                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                .build();
+        detector = FaceDetection.getClient(options);
         Uri uriCapture = FileProvider.getUriForFile(
                 context,
                 context.getPackageName() + ".fileprovider",
@@ -37,5 +41,9 @@ public class ImageParser {
 
     public Task<Text> parseImageText(){
         return recognizer.process(image);
+    }
+
+    public Task<List<Face>> extractFace(){
+        return detector.process(image);
     }
 }
--- app/src/main/java/com/example/calmsidocr/ResultActivity.java
+++ app/src/main/java/com/example/calmsidocr/ResultActivity.java
@@ -3,12 +3,19 @@ package com.example.calmsidocr;
 import static com.example.calmsidocr.Constants.KEY_FILE;
 import static com.example.calmsidocr.Constants.MSG_NA;
 import static com.example.calmsidocr.Constants.PAT_NRIC;
+import static com.example.calmsidocr.Constants.PAT_PASSPORT;
 import static com.example.calmsidocr.Constants.TAG_BUNDLE;
 import static com.example.calmsidocr.Constants.TAG_EVENT;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +24,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.exifinterface.media.ExifInterface;
+
+import com.google.mlkit.vision.face.Face;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,7 +44,7 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -43,6 +53,8 @@ public class ResultActivity extends AppCompatActivity {
         // Bind view components.
         TextView txtResult = findViewById(R.id.txtResultContent);
         TextView txtDocID = findViewById(R.id.txtDocIDContent);
+        TextView txtPhotoContent = findViewById(R.id.txtPhotoContent);
+        ImageView imagePhotoContent = findViewById(R.id.imagePhotoContent);
 
         String incoming = getIntent().getStringExtra(KEY_FILE);
         Log.d(TAG_BUNDLE, "ResultActivity: " + (incoming != null ? incoming : "null"));
@@ -63,6 +75,32 @@ public class ResultActivity extends AppCompatActivity {
                         })
                         .addOnFailureListener(e -> {
                             Log.e(TAG_EVENT, "ERROR: parseImageText FailureListener\n" + e);
+                        });
+
+                imageParser.extractFace()
+                        .addOnSuccessListener(faces -> {
+                            if (!faces.isEmpty()) {
+                                Rect maxBBox = null;
+                                int maxArea = 0;
+                                for (Face face : faces) {
+                                    Rect bBox = face.getBoundingBox();
+                                    int area = getBoundingBoxArea(bBox);
+                                    if (area > maxArea) {maxBBox = bBox; maxArea = area;}
+                                }
+                                assert maxBBox != null;
+                                try {
+                                    imagePhotoContent.setImageBitmap(getFaceBitmap(fileCapture, maxBBox));
+                                    txtPhotoContent.setVisibility(View.INVISIBLE);
+                                } catch (IOException e) {
+                                    txtPhotoContent.setText(MSG_NA);
+                                }
+                            } else {
+                                txtPhotoContent.setText(MSG_NA);
+                            }
+                            Log.d(TAG_EVENT, "CALLBACK: extractFace SuccessListener");
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.e(TAG_EVENT, "ERROR: extractFace FailureListener\n" + e);
                         });
             } catch (IOException e) {
                 Log.e(TAG_EVENT, "EXCEPTION: ImageParser constructor\n" + e);
@@ -85,12 +123,42 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     private String getDocumentID(String inputStr) {
-        Pattern patNRIC = Pattern.compile(PAT_NRIC, Pattern.CASE_INSENSITIVE);
+        Pattern patNRIC = Pattern.compile(PAT_NRIC);
+        Pattern patPassport = Pattern.compile(PAT_PASSPORT);
         Matcher matNRIC = patNRIC.matcher(inputStr);
+        Matcher matPassport = patPassport.matcher(inputStr);
         if (matNRIC.find()){
-            return matNRIC.group(0);
-        } else {
-            return "";
+            return matNRIC.group();
+        } else if (matPassport.find()) {
+            if (matPassport.group().length() <= 9) {return matPassport.group();}
         }
+        return "";
+    }
+
+    private int getBoundingBoxArea(Rect bBox) {
+        return bBox.width() * bBox.height();
+    }
+
+    private Bitmap getFaceBitmap(File fileCapture, Rect bBox) throws IOException {
+        ExifInterface exif = new ExifInterface(fileCapture);
+        Matrix matrix = new Matrix();
+        matrix.postRotate(exif.getRotationDegrees());
+        Bitmap bitmap = BitmapFactory.decodeFile(fileCapture.getAbsolutePath());
+        Bitmap rotated = Bitmap.createBitmap(
+                bitmap,
+                0,
+                0,
+                bitmap.getWidth(),
+                bitmap.getHeight(),
+                matrix,
+                true
+        );
+        return Bitmap.createBitmap(
+                rotated,
+                bBox.left,
+                bBox.top,
+                bBox.width(),
+                bBox.height()
+        );
     }
 }
