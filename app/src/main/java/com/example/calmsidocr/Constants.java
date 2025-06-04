f80c2a9ade24157befb85db0576c399ebce86f5a
cf.le3 <cf.lee0064@gmail.com>
2025-06-04 15:58:56 +0800

    Add new pattern strings for new document types

--- app/src/main/java/com/example/calmsidocr/Constants.java
+++ app/src/main/java/com/example/calmsidocr/Constants.java
@@ -2,10 +2,10 @@ package com.example.calmsidocr;
 
 public final class Constants {
     private Constants() {}
-
     public static final String KEY_FILE = "KEY_F";
-    public static final String PAT_NRIC = "([0-9]{6}-[0-9]{2}-[0-9]{4}|[0-9]{12})";
-    public static final String PAT_PASSPORT = "[A-Z]*[0-9]+[A-Z]*";
+    public static final String PAT_NRIC = "\b[0-9]{6}-[0-9]{2}-[0-9]{4}\b";
+    public static final String PAT_PASSPORT = "\b[A-Z]*[0-9]+[A-Z]*\b";
+    public static final String PAT_LICENSE = "\b[0-9]{12}\b";
     public static final String MSG_NA = "Not found.";
     public static final String TAG_EVENT = "TAG_E";
     public static final String TAG_BUNDLE = "TAG_B";
